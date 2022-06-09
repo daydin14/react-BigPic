6cfaf3d0f6626f18c8be7bd786795ec9384f1515
David Aydin <david.aydin6@gmail.com>
2022-06-09 00:32:50 -0400

    Map function

--- big-pic/src/App.js
+++ big-pic/src/App.js
@@ -11,8 +11,25 @@ export default function App() {
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
 
+  const handleClick = (imgUrl) => {
+    setBigImage(imgUrl);
+  };
+
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+
+  const images = imagesArr.map((image, index) => {
+    return (
+      <img
+        className="thumb"
+        id={image.city}
+        src={image.img}
+        alt={image.city}
+        key={index}
+        onClick={() => handleClick(image.img)}
+      />
+    );
+  });
 
   return (
     <div className="App">
