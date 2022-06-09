62ee803c69cab965347dc3799e15472f98c6e636
David Aydin <david.aydin6@gmail.com>
2022-06-09 00:35:31 -0400

    thumbnail images to pick from

--- big-pic/src/App.js
+++ big-pic/src/App.js
@@ -35,8 +35,8 @@ export default function App() {
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        <div id="thumbnails">{images}</div>
+
         {/* <img src="" id="bigimage" alt='bigImage'/> */}
       </div>
     </div>
